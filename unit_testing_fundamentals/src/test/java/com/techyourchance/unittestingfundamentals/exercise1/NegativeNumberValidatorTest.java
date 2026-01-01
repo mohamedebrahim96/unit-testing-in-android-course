ae95565423c07c82b22cb47c2364bcc3c8fe5157
Mohamed Ebrahim <ebrahimm131@gmail.com>
2026-01-01 19:20:43 +0400

    Fixes negative number validation logic
    Corrects the negative number validator to exclude zero
    from being considered a negative number.
    
    Adds unit tests to verify the corrected behavior.

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class NegativeNumberValidatorTest {
 
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1(){
+        boolean result = SUT.isNegative(-1);
+        assertTrue(result);
+    }
+
+    @Test
+    public void test2(){
+        boolean result = SUT.isNegative(0);
+        assertFalse(result);
+    }
+
+    @Test
+    public void test3(){
+        boolean result = SUT.isNegative(1);
+        assertFalse(result);
+    }
 }
